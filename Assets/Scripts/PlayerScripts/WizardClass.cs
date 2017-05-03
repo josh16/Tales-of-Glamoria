ab4536532fb72883a4e7217737e3a0719faaa6b6
josh16 <josh.randall16@gmail.com>
2017-05-02 22:25:24 -0400

    Base Class Character Script created
    -Created the BaseClass script
    -Created the Wizard, Necromancer, Warrior, and Crusader class scripts
    -All the character scripts will derive from the "Base Character Class
    script"

---
+++ Assets/Scripts/PlayerScripts/WizardClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardClass : BaseClass {
+
+	
+    //Skills
+    
+    
+    
+    
+    
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+
+        
+       
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
